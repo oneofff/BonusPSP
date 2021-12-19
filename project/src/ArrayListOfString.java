8f2e2f35133e1972e215b38cb169a280edafb665
Stas <stanislove.korol@gmail.com>
2021-12-19 18:14:15 +0300

    clean up code

--- project/src/ArrayListOfString.java
+++ project/src/ArrayListOfString.java
@@ -1,32 +1,48 @@
 import java.util.*;
 
 public class ArrayListOfString {
-    private List<String> arrayList;
-    void add(String str){
+    private final List<String> arrayList;
+
+    void add(String str) {
         try {
             arrayList.add(str);
-        }catch (NullPointerException ex){
+        } catch (NullPointerException ex) {
             System.out.println("Error, string is null");
-            ex.printStackTrace();
         }
 
     }
-    void delete(String str){
-        try {
-            arrayList.remove(str);
-        }catch (NullPointerException ex){
-            System.out.println("Error, string is null");
-            ex.printStackTrace();
-        }
-    }
-    void delete(int index)
-    {
-        try {
-            arrayList.remove(index);
 
-        }catch (IndexOutOfBoundsException ex){
+    public ArrayListOfString() {
+        arrayList = new ArrayList<>();
+    }
+
+    boolean delete(String str) {
+        try {
+            return arrayList.remove(str);
+        } catch (NullPointerException ex) {
+            System.out.println("Error, string is null");
+        }
+        return false;
+    }
+
+    String delete(int index) {
+        try {
+            return arrayList.remove(index);
+        } catch (IndexOutOfBoundsException ex) {
             System.out.println("Error, index is incorrect");
-            ex.printStackTrace();
         }
+        return null;
+    }
+
+
+    @Override
+    public String toString() {
+        return "["
+                + arrayList +
+                ']';
+    }
+
+    public int getLength() {
+        return arrayList.size();
     }
 }
--- project/src/Main.java
+++ project/src/Main.java
@@ -1,5 +1,78 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+
+    private static final ArrayListOfString strings = new ArrayListOfString();
+
+    private static final Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("привет мир!");
+        while (true) {
+            System.out.println();
+            showMenu();
+            String choice = in.next();
+            switch (choice) {
+                case "1" -> add();
+                case "2" -> delete();
+                case "0" -> System.exit(0);
+            }
+        }
     }
+
+    private static void showMenu() {
+        System.out.println("1. Добавление нового объекта");
+        System.out.println("2. Удаление нового объекта");
+        System.out.println("3. Поиск одинаковых элементов с подсчетом совпадений");
+        System.out.println("4. Выгрузка в xml-файл");
+        System.out.println("5. Реверс всех строк, входящих в коллекцию");
+        System.out.println("6. Статистика по всем символам, содержащимся в строках коллекции");
+        System.out.println("7. Поиск подстроки в строках коллекции");
+        System.out.println("8. Инициализация листа по текстовому файлу и вывод содержимого коллекции на экран");
+        System.out.println("9. Сравнение строк");
+        System.out.println("10. Длины строк входящих в коллекцию");
+        System.out.println("0. Выход");
+    }
+
+    private static void delete() {
+        int length = strings.getLength();
+        if (length <= 0) {
+            System.out.println("Массива строк не существует");
+            return;
+        }
+        System.out.println("1. Удаление по индексу");
+        System.out.println("2. Удаление по содержимому строки");
+        String choose = in.next();
+        if (choose.equals("1")) {
+            System.out.println("Введите индекс от 0 до " + (strings.getLength() - 1));
+            try {
+                int index = in.nextInt();
+                String deletedString = strings.delete(index);
+                if (deletedString == null) {
+                    return;
+                }
+                System.out.println("Строка [" + deletedString + "] удалена");
+                System.out.println("Вся коллекция: " + strings);
+            } catch (InputMismatchException ex) {
+                System.out.println("Неверный формат числа");
+            }
+        } else if (choose.equals("2")) {
+            System.out.println("Введите строку");
+            String deletedString = in.next();
+            boolean isDeleted = strings.delete(deletedString);
+            if (isDeleted) {
+                System.out.println("Строка [" + deletedString + "] успешно удалена");
+            }
+            System.out.println("Вся коллекция: " + strings);
+        }
+    }
+
+    private static void add() {
+        System.out.println("Введите строку");
+        String newString = in.next();
+        strings.add(newString);
+        System.out.println("Строка добавлена");
+        System.out.println("Вся коллекция: " + strings);
+    }
+
 }
