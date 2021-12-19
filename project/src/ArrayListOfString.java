b03b60ab32a69bc16fb35f66e881b7cccb5db9d8
Stas <stanislove.korol@gmail.com>
2021-12-19 16:54:02 +0300

    added delete strings

--- project/src/ArrayListOfString.java
+++ project/src/ArrayListOfString.java
@@ -3,6 +3,30 @@ import java.util.*;
 public class ArrayListOfString {
     private List<String> arrayList;
     void add(String str){
-        arrayList.add(str);
+        try {
+            arrayList.add(str);
+        }catch (NullPointerException ex){
+            System.out.println("Error, string is null");
+            ex.printStackTrace();
+        }
+
+    }
+    void delete(String str){
+        try {
+            arrayList.remove(str);
+        }catch (NullPointerException ex){
+            System.out.println("Error, string is null");
+            ex.printStackTrace();
+        }
+    }
+    void delete(int index)
+    {
+        try {
+            arrayList.remove(index);
+
+        }catch (IndexOutOfBoundsException ex){
+            System.out.println("Error, index is incorrect");
+            ex.printStackTrace();
+        }
     }
 }
